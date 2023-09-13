2c046ff98de62180b5571daf1ef091c965700534
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2023-09-14 08:44:47 +0900

    add f_03.cpp file

---
+++ f_03.cpp
@@ -0,0 +1,21 @@
+//ポインタと参照
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    x = 5;
+
+    //&をつけるとポインタとなる
+    cout <<"x: "<< x << endl;
+    cout <<"&x: "<< &x << endl;
+    
+    //ポインタを格納するための変数
+    int *p_x;//慣例としてポインタ変数の宣言と代入部分は分けて書く（一緒に書くと正直名に書いてあるかわかんないから）
+    p_x = &x;
+
+    cout << "p_x: " << p_x <<endl;
+    cout << "*p_x: " << *p_x <<endl;
+
+    return 0;
+}
